0a2140e313d4c1d4660ae761ddf75e9912a6dd3e
dgross881 <dgross881@gmail.com>
2024-09-02 11:02:38 +0800

    Added practice functions to learn how to work with string in elixir

---
+++ lib/string_practice.ex
@@ -0,0 +1,73 @@
+defmodule ELixirPractice.StringPractice do
+   @doc """
+   Exercices with strings.
+   https://hexdocs.pm/elixir/String.html
+   programming elixir books chapter 11 String and Binary operations page 132 
+   """
+
+  # reverse the string example: the should become eht 
+  def reverse_string(_str) do
+  end
+
+  # count the amount of vowels in a string a, e, i, o, u example: "hello" should return 2
+  def count_vowels(str) do
+  end
+
+
+  # capitalize the first letter of each word in a string example: "hello world" should return "Hello World"
+  def capitalize_words(str) do
+  end
+
+
+  # replace all instances of a substring with another substring example: "hello world" "world" "universe" should return "hello universe"
+  def replace_substring(str, target, replacement) do
+  end
+
+
+  # find the longest word in a string example: "programming elixir" should return programming
+  def find_longest_word(str) do
+  end
+
+
+  # remove all whitespace in a string example: "hello world" should return "helloworld"
+  def remove_whitespace(str) do
+  end
+
+  # count the amount of words in a string example: "hello world" should return 2
+  def count_words(str) do
+  end
+
+
+  # check if a string contains a substring example: "hello world" "world" should return true "hello dave" should return false
+  def contains_substring?(str, substring) do
+  end
+
+  # repeat a string n times example: "hello" 3 should return "hello hello hello"
+  def repeat_string(str, n) do
+  end
+
+
+  # convert a string to uppercase example: "hello" should return "HELLO"
+  def to_uppercase(str) do
+  end
+
+  # split a string into words example: "hello world" should return ["hello", "world"]
+  def split_into_words(str) do
+  end
+
+  # remove a character from a string example: "hello" "l" should return "heo"
+  def remove_character(str, char) do
+  end
+
+  # count the amount of occurrences of a character in a string example: "hello" "l" should return 2
+  def count_char_occurrences(str, char) do
+  end
+
+  # remove all duplicates from a string example: "hello world" should return "helo wrd"
+  def remove_duplicates(str) do
+  end
+
+  # remove all non-alphanumeric characters from a string example: "hello world!" should return "hello world"
+  def remove_non_alphanumeric(str) do
+  end
+end
